60ff2640ce35dd5b3121a674e5de8c0938bc0516
roshith2001 <roshithj@gmail.com>
2024-10-15 16:20:34 +0530

    Second Commit

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const PORT = 3001
 const MongoClient = require('mongodb').MongoClient
 const { ObjectId } = require('mongodb')
 
-const url = ''
+const url = 'mongodb+srv://devagiri:kolathara1@devagiri.52w6g.mongodb.net/?retryWrites=true&w=majority&appName=Devagiri'
 const dbName = 'Exam'
 let db
 
@@ -18,7 +18,30 @@ MongoClient.connect(url)
     console.log('Connected to MongoDB')
     db = client.db(dbName)
     
-    
+    app.post('/addtask', async(req, res) => {
+        try{
+          // Use data from request body instead of hardcoded values
+          const task = req.body
+          const result = await db.collection('tasks').insertOne(task);
+          res.send({message: 'Task created successfully'})
+        }
+        catch(err){
+          console.error('Error creating task:', err)
+          res.status(500).send({message: 'Error creating task', error: err.message})
+        }
+    })
+
+    app.get('/tasks', async(req, res) => {
+        try{
+            const response = await db.collection('tasks').find({}).toArray();
+            console.log(response)
+            res.send(response).status(200)
+        }
+        catch(err){
+            console.log(err)
+            res.status(500).send({message: `Some Error occured ${err}`})
+        }
+    })
     
     app.listen(PORT, () => {
       console.log(`Server live on http://localhost:${PORT}`)
